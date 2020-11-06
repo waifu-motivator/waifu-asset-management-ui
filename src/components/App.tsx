00ed226d050f2549bef13a8da3b7b5e90470748a
DATBOI <alexsimons9999@gmail.com>
2020-11-06 10:58:28 -0600

    Added react router

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,11 +2,28 @@ import './App.css';
 import {withAuthenticator} from "@aws-amplify/ui-react";
 import React from 'react';
 import Header from "./header/Header";
+import {Route, Switch, Redirect, Router} from 'react-router-dom';
+import {createMemoryHistory} from 'history'
+
+const history = createMemoryHistory();
 
 const App = () => (
-  <div className="App">
-    <Header/>
-  </div>
+  <Router history={history}>
+    <div className="App">
+      <Header/>
+      <Switch>
+        <Route path='/'>
+          <div>Home</div>
+        </Route>
+        <Route path='/upload'>
+          <div>Upload</div>
+        </Route>
+        <Route path='/oauth/callback'>
+          <Redirect to={"/"}/>
+        </Route>
+      </Switch>
+    </div>
+  </Router>
 );
 
 export default withAuthenticator(App);
