711a351eeba9bcd854a4ca46e299e44e301b677b
DATBOI <alexsimons9999@gmail.com>
2020-11-08 11:42:24 -0600

    Added crude infinite scroll

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,10 +1,10 @@
-import React, {FC} from 'react';
-import clsx from "clsx";
+import React, {FC, useState} from 'react';
 import {makeStyles} from "@material-ui/core/styles";
 import {Box, Container, Grid, Link, Paper, Typography} from "@material-ui/core";
 import {useSelector} from "react-redux";
 import {selectVisualAssetState} from "../reducers";
 import {AWSConfig} from "../config/AwsConfig";
+import InfiniteScroll from "./util/InfiniteScroll";
 
 const useStyles = makeStyles((theme) => ({
   content: {
@@ -42,27 +42,49 @@ function Copyright() {
 
 const s3Url = `https://${AWSConfig.Storage.AWSS3.bucket}.s3.amazonaws.com/`
 
+const waifuPerPage = 10;
 const Dashboard: FC = () => {
   const classes = useStyles();
+
   const {s3List} = useSelector(selectVisualAssetState);
-  const viewedS3Items = s3List.slice(0, 25)
-  return (
+  const [assetIndex, setAssetIndex] = useState(waifuPerPage);
+
+  const viewedS3Items = s3List.slice(0, assetIndex)
+  const hasMore = s3List.length > viewedS3Items.length;
+  const fetchData = () => {
+    if(hasMore){
+      setAssetIndex(prevState => {
+        const nextIndex = prevState + waifuPerPage;
+        return nextIndex > s3List.length ? s3List.length : nextIndex;
+      })
+    }
+
+  };
+
+  return !s3List.length ? (<></>) : (
     <Container className={classes.container}>
-      <Grid container spacing={3}>
-        {
-          viewedS3Items.map(s3Item => (
-            <Grid item key={s3Item.key}>
-              <Paper className={classes.paper}>
-                <img src={`${s3Url}${s3Item.key}`}
-                     style={{
-                       borderRadius: '0.5rem'
-                     }}
-                     alt={s3Item.key} />
-              </Paper>
-            </Grid>
-          ))
-        }
-      </Grid>
+      <InfiniteScroll
+                      loadMore={fetchData}
+                      hasMore={hasMore}
+                      loadMoreDisplay={<h3>Hang tight Senpai...</h3>}
+      >
+        <Grid container spacing={3}>
+
+          {
+            viewedS3Items.map(s3Item => (
+              <Grid item key={s3Item.key}>
+                <Paper className={classes.paper}>
+                  <img src={`${s3Url}${s3Item.key}`}
+                       style={{
+                         borderRadius: '0.5rem'
+                       }}
+                       alt={s3Item.key}/>
+                </Paper>
+              </Grid>
+            ))
+          }
+        </Grid>
+      </InfiniteScroll>
       <Box pt={4}>
         <Copyright/>
       </Box>
---
+++ src/components/util/InfiniteScroll.tsx
@@ -0,0 +1,50 @@
+import React, {FC, useEffect, useRef, useState} from 'react';
+
+interface Props {
+  loadMore: (pageNumber: number) => void;
+  hasMore: boolean;
+  loadMoreDisplay: JSX.Element;
+}
+
+const InfiniteScroll: FC<Props> = ({
+                                     children,
+                                     hasMore,
+                                     loadMoreDisplay,
+                                     loadMore
+                                   }) => {
+  const [page, setPage] = useState(1);
+  const loader = useRef<HTMLDivElement | null>(null);
+
+  const handleObserver = (entities: any[]) => {
+    const target = entities[0];
+    if (target.isIntersecting) {
+      loadMore(page + 1)
+    }
+  }
+
+
+  useEffect(() => {
+    console.log('loader', loader);
+    const options = {
+      root: null,
+      rootMargin: "20px",
+      threshold: 1.0
+    };
+    const observer = new IntersectionObserver(handleObserver, options);
+    if (loader.current) {
+      observer.observe(loader.current)
+    }
+  }, []);
+
+
+  return (
+    <>
+      {children}
+      <div ref={loader}>
+        {hasMore && loadMoreDisplay}
+      </div>
+    </>
+  )
+}
+
+export default InfiniteScroll;
